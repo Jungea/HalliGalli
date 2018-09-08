f7d78ea3f0a8a54b260b5158cc593520c1ee105a
Jungea <dmsdo2241@naver.com>
2018-09-08 23:15:28 +0900

    프레임 생성

--- src/halligalli/HGClient.java
+++ src/halligalli/HGClient.java
@@ -1,10 +1,21 @@
 package halligalli;
 
-public class HGClient {
+import javax.swing.JFrame;
 
+public class HGClient extends JFrame {
+
+	public HGClient() {
+		setTitle("Halli Galli");
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		HGClient client = new HGClient();
+		client.setSize(800,600);
+		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.setVisible(true);
 	}
 
 }
